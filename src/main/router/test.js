404dd96addd96ec197bbb96139d77229dda5917f
youliso <youliso@hotmail.com>
2020-04-03 11:04:26 +0800

    添加 async-lock

--- src/main/router/test.js
+++ src/main/router/test.js
@@ -1,14 +1,9 @@
 'use strict';
-const _ = require('../../resources/utils/original');
 const Router = require('koa-router');
 const test = new Router();
 let va = 0;
 test.get('/i', async (ctx, next) => {
-    ctx.body = await _.lock.acquire('user_add', async () => {
-        console.log('start');
-    }).then(() => {
-        return _.success('注册成功');
-    });
+    ctx.body = ctx.result.success(va++);
     await next();
 });
 
