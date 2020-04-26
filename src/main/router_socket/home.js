6d70e09418f049e857936386f873a2012540d22c
youliso <youliso@hotmail.com>
2020-04-26 17:12:44 +0800

    修复错误

--- src/main/router_socket/home.js
+++ src/main/router_socket/home.js
@@ -3,10 +3,6 @@ const _ = require('../../resources/utils/original');
 
 class home {
     index(client, ctx) {
-        let users = [];
-        for (let i in ctx.users) {
-            users.push(ctx.users[i].family);
-        }
         ctx.broadcast(_.WsSuccess('ok', ctx.result, {test: "test"}));
     }
 }
