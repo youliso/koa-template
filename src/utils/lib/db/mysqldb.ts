f8e00a788225b6c6ab0a64e61eba1734c5ea7f8a
youliso <youliso@hotmail.com>
2020-09-13 20:47:52 +0800

    mysql连接池问题

--- src/utils/lib/db/mysqldb.ts
+++ src/utils/lib/db/mysqldb.ts
@@ -7,101 +7,93 @@ export default class Mysqldb {
         this.dbClient = createPool(db);
     }
 
-    //将结果已对象数组返回
-    row(sql: string, params: object) {
-        const promisePool = this.dbClient.promise();
-        return new Promise((resolve, reject) => {
-            promisePool.query(sql, params)
-                .then(res => {
-                    resolve(res);
-                    // @ts-ignore
-                    promisePool.release();
-                })
-                .catch(err => {
-                    reject(err);
-                    // @ts-ignore
-                    promisePool.release();
-                });
-        });
-    }
-
     //返回一个对象
     first(sql: string, params: object) {
-        const promisePool = this.dbClient.promise();
         return new Promise((resolve, reject) => {
-            promisePool.query(sql, params)
-                .then(res => {
-                    resolve(res[0] || null);
-                    // @ts-ignore
-                    promisePool.release();
-                })
-                .catch(err => {
+            this.dbClient.getConnection(async (err, conn) => {
+                if (err) {
                     reject(err);
-                    // @ts-ignore
-                    promisePool.release();
-                });
+                    return;
+                }
+                conn.promise().query(sql, params)
+                    .then(res => {
+                        resolve(res[0] || null);
+                        this.dbClient.releaseConnection(conn);
+                    })
+                    .catch(e => {
+                        reject(e);
+                        this.dbClient.releaseConnection(conn);
+                    });
+            });
         });
     }
 
     //返回单个查询结果
     single(sql: string, params: object) {
-        console.log(sql, params)
-        const promisePool = this.dbClient.promise();
         return new Promise((resolve, reject) => {
-            promisePool.query(sql, params)
-                .then(res => {
-                    for (let i in res[0]) {
-                        resolve(res[0][i] || null);
-                        // @ts-ignore
-                        promisePool.release();
-                        return;
-                    }
-                    resolve(null);
-                    // @ts-ignore
-                    promisePool.release();
-                })
-                .catch(err => {
+            this.dbClient.getConnection(async (err, conn) => {
+                if (err) {
                     reject(err);
-                    // @ts-ignore
-                    promisePool.release();
-                });
+                    return;
+                }
+                conn.promise().query(sql, params)
+                    .then(res => {
+                        for (let i in res[0]) {
+                            resolve(res[0][i] || null);
+                            this.dbClient.releaseConnection(conn);
+                            return;
+                        }
+                        resolve(null);
+                        this.dbClient.releaseConnection(conn);
+                    })
+                    .catch(err => {
+                        reject(err);
+                        this.dbClient.releaseConnection(conn);
+                    });
+            });
         });
     }
 
     //执行代码，返回执行结果
     query(sql: string, params: object) {
-        const promisePool = this.dbClient.promise();
         return new Promise((resolve, reject) => {
-            promisePool.query(sql, params)
-                .then(res => {
-                    resolve(res);
-                    // @ts-ignore
-                    promisePool.release();
-                })
-                .catch(err => {
+            this.dbClient.getConnection(async (err, conn) => {
+                if (err) {
                     reject(err);
-                    // @ts-ignore
-                    promisePool.release();
-                });
+                    return;
+                }
+                conn.promise().query(sql, params)
+                    .then(res => {
+                        resolve(res);
+                        this.dbClient.releaseConnection(conn);
+                    })
+                    .catch(err => {
+                        reject(err);
+                        this.dbClient.releaseConnection(conn);
+                    });
+            });
         });
     }
 
     //执行代码，返回执行结果
     execute(sql: string, params: object) {
-        const promisePool = this.dbClient.promise();
-        return new Promise(async (resolve, reject) => {
-            promisePool.execute(sql, params)
-                .then(res => {
-                    resolve(res);
-                    // @ts-ignore
-                    promisePool.release();
-                })
-                .catch(err => {
+        return new Promise((resolve, reject) => {
+            this.dbClient.getConnection(async (err, conn) => {
+                if (err) {
                     reject(err);
-                    // @ts-ignore
-                    promisePool.release();
-                });
-        })
+                    return;
+                }
+                conn.promise().execute(sql, params)
+                    .then(res => {
+                        resolve(res);
+                        this.dbClient.releaseConnection(conn);
+                    })
+                    .catch(err => {
+                        reject(err);
+                        this.dbClient.releaseConnection(conn);
+                    });
+            });
+        });
     }
 
     /**
